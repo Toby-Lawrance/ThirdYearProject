de8a1162b026c06dbce6420d6385935559538ee4
Toby Lawrance <toby.lawrance@gmail.com>
2019-10-29 13:52:47 +0000

    Small tweak to setup script, cpp_pubsub built on ubuntu server

--- TurtlebotRos2SetupScript.sh
+++ TurtlebotRos2SetupScript.sh
@@ -7,7 +7,7 @@ sudo fallocate -l 1G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+sudo bash -s 'echo "/swapfile swap swap defaults 0 0" >> /etc/fstab'
 
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
