5fd736fd786e3fb43306e8a833831044cc54af2d
Toby <toby.lawrance@gmail.com>
2019-10-29 13:29:44 +0000

    Fixed conflict with the local_setup.sh for cpp_pubsub

--- ros2TurtleBot3_ws/build/cpp_pubsub/ament_cmake_environment_hooks/local_setup.sh
+++ ros2TurtleBot3_ws/build/cpp_pubsub/ament_cmake_environment_hooks/local_setup.sh
@@ -2,11 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-<<<<<<< HEAD
-: ${AMENT_CURRENT_PREFIX:="/home/toby/ThirdYearProject/ros2TurtleBot3_ws/install/cpp_pubsub"}
-=======
-: ${AMENT_CURRENT_PREFIX:="/home/portia/ThirdYearProject/ros2TurtleBot3_ws/install/cpp_pubsub"}
->>>>>>> 2bfd020d511dce9e8de31ce95719fda3d0662758
+${AMENT_CURRENT_PREFIX:="/home/toby/ThirdYearProject/ros2TurtleBot3_ws/install/cpp_pubsub"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
